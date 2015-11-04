0e15785bea0218a71db175c9024bb19bbf87d3df
mydongistiny <jaysonedson@gmail.com>
2015-11-04 11:18:21 -0800

    angler: Add new default dpi options

--- device.mk
+++ device.mk
@@ -213,8 +213,10 @@ DEVICE_PACKAGE_OVERLAYS := \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.opengles.version=196609
 
+# DPI Settings
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=560
+    ro.sf.lcd_density=560 \
+    persist.sys.lcd_density=560
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.hwc.mdpcomp.enable=true \
